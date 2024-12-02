c562f4eebdc3d4c2f3ccd235189b46d2012521e3
Kashu-patidar <kashishpatidar4444@gmail.com>
2024-12-02 13:19:23 +0530

    calcutare

---
+++ calcu.js
@@ -0,0 +1,56 @@
+var x,y,z
+function input1(){
+    document.getElementById("input").value +="1"
+}
+function input2(){
+    document.getElementById("input").value +="2"
+}
+
+function input3(){
+    document.getElementById("input").value +="3"
+}
+
+function input4(){
+    document.getElementById("input").value +="4"
+}
+
+function input5(){
+    document.getElementById("input").value +="5"
+}
+
+function input6(){
+    document.getElementById("input").value +="6"
+}
+
+function input7(){
+    document.getElementById("input").value +="7"
+}
+
+function input8(){
+    document.getElementById("input").value +="8"
+}
+
+function input9(){
+    document.getElementById("input").value +="9"
+}
+function input0(){
+    document.getElementById("input").value +="0"
+}
+function input(){
+    document.getElementById("input").value +="+"
+}
+function input(){
+    document.getElementById("input").value +="-"
+}
+function input(){
+    document.getElementById("input").value +="*"
+}
+function input(){
+    document.getElementById("input").value +="/"
+}
+function calculate(){
+    document.getElementById("input").value=eval(document.getElementById("inputbox").value)
+}
+
+
+
